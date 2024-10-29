0d61b1d032f610ad749fcc32fd5562971c8980cb
Ishika Agarwal <boilermakerishika@gmail.com>
2024-10-29 16:19:46 -0500

    chunk paper

--- paper_details.py
+++ paper_details.py
@@ -3,6 +3,8 @@ class Paper:
         self.text = text
         self.chunk()
     
-    def chunk(self):
-        self.chunks = None
-        raise NotImplemented
+    def chunk(self, chunk_size=5):
+        self.chunks = []
+        sentences = self.text.split('.')
+        for i in range(0, len(sentences), chunk_size):
+            self.chunk.append('. '.join(sentences[i:i+chunk_size]))
