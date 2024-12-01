d954899a7f45c496f6b3cae2a033b00a1af6cb0a
Shivam Agarwal <shivama2@gh123.hsn.cm.delta.internal.ncsa.edu>
2024-12-01 13:55:46 -0600

    better evals

--- evaluators/FENICE/run.sh
+++ evaluators/FENICE/run.sh
@@ -4,6 +4,8 @@ export HF_DATASETS_CACHE="/work/nvme/bcaq/shivama2/hf_cache_rlhf/"
 
 export TRITON_LIBCUDA_PATH=/usr/local/cuda/compat/lib.real #WORKS MUST DO! 
 
-python run.py --results_path /work/nvme/bcaq/shivama2/new_tod/tree-of-debate/baselines/results_split.csv
-python run.py --results_path /work/nvme/bcaq/shivama2/new_tod/tree-of-debate/baselines/results_prompt_intro.csv
+python run.py --results_path /work/nvme/bcaq/shivama2/new_tod/tree-of-debate/baselines/results_split.csv --author_name 'author 0'
+python run.py --results_path /work/nvme/bcaq/shivama2/new_tod/tree-of-debate/baselines/results_split.csv --author_name 'author 1'
+python run.py --results_path /work/nvme/bcaq/shivama2/new_tod/tree-of-debate/baselines/results_prompt_intro.csv --author_name  'author 0'
+python run.py --results_path /work/nvme/bcaq/shivama2/new_tod/tree-of-debate/baselines/results_prompt_intro.csv --author_name  'author 1'
 
